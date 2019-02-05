cc04843edbbb3affd26e9fd5db9d6fe82d5a8ffb
albuquerque2012 <albuquerque.rodrigo@gmail.com>
2019-02-05 20:40:32 -0300

    exercicio_final

--- mod 2 aula 1/script.R
+++ mod 2 aula 1/script.R
@@ -63,3 +63,46 @@ ggsave("homoced.png", width=6, height=4, units="in")
 
 exercicio <- read.csv("QOGdataSave.csv", sep =";", stringsAsFactors=FALSE)
 str(exercicio)
+exerciciomanipuladov1 <- exercicio[complete.cases(total_export)]
+
+#### fazendo o exercicio
+library(readr)
+library(stringr)
+library(stargazer)
+library(ggplot2)
+exercicio <- read.csv("QOGdataSave.csv", sep =";", stringsAsFactors=FALSE)
+pol_cor <- exercicio$political_corruption
+tot_exp <- exercicio$total_export
+epi <- exercicio$environmental_performance_index
+exercicio$total_export <- as.factor(exercicio$total_export)
+exercicio$environmental_performance_index <- as.factor(exercicio$environmental_performance_index)
+exercicio$political_corruption <- as.factor(exercicio$political_corruption)
+ggplot(data = exercicio, aes(x = political_corruption, y = environmental_performance_index))+ # dev variáveis e eixos
+  geom_point()+ # gráf tipo ponto
+  geom_smooth(method = "lm") # reta de regressão linear
+ggsave("reg_exec_1.png", width=6, height=4, units="in")
+ggplot(data = exercicio, aes(x = total_export, y = environmental_performance_index))+ # dev variáveis e eixos
+  geom_point()+ # gráf tipo ponto
+  geom_smooth(method = "lm") # reta de regressão linear
+ggsave("reg_exec_2.png", width=6, height=4, units="in")
+
+
+modelo_reg <- lm(environmental_performance_index ~ # VD
+                total_export +           # VIs
+                political_corruption,
+              data = exercicio)
+summary(modelo_reg)
+
+exercicio$Predito <- predict(modelo_reg)
+exercicio$Residuo <- exercicio$total_export - exercicio$Predito
+exercicio$Predito <- predict(modelo_reg)
+exercicio$Residuo <- exercicio$political_corruption - exercicio$Predito
+
+summary(exercicio$Residuo)
+
+# gráfico de dispersao dos valores preditos x residuos
+ggplot(data = exercicio, aes(x = Predito, y = Residuo)) +
+  geom_point() +
+  labs( x ="Valores Preditos (modelo_reg)", y = "Residuos (modelo_reg)") +
+  geom_hline(yintercept = 0, linetype = "dashed", color = "blue")
+ggsave("hom_exec.png", width=6, height=4, units="in")
