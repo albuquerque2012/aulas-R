bd2b770b3456d10fb0b08b80e093178b7f240628
albuquerque2012 <albuquerque.rodrigo@gmail.com>
2019-02-06 09:50:46 -0300

    exercicios

--- mod 2 aula 1/script.R
+++ mod 2 aula 1/script.R
@@ -70,39 +70,112 @@ library(readr)
 library(stringr)
 library(stargazer)
 library(ggplot2)
+library(dplyr)
+install.packages("tidyr")
+library(tidyr)
 exercicio <- read.csv("QOGdataSave.csv", sep =";", stringsAsFactors=FALSE)
-pol_cor <- exercicio$political_corruption
-tot_exp <- exercicio$total_export
-epi <- exercicio$environmental_performance_index
-exercicio$total_export <- as.factor(exercicio$total_export)
-exercicio$environmental_performance_index <- as.factor(exercicio$environmental_performance_index)
-exercicio$political_corruption <- as.factor(exercicio$political_corruption)
-ggplot(data = exercicio, aes(x = political_corruption, y = environmental_performance_index))+ # dev variáveis e eixos
-  geom_point()+ # gráf tipo ponto
-  geom_smooth(method = "lm") # reta de regressão linear
-ggsave("reg_exec_1.png", width=6, height=4, units="in")
-ggplot(data = exercicio, aes(x = total_export, y = environmental_performance_index))+ # dev variáveis e eixos
-  geom_point()+ # gráf tipo ponto
-  geom_smooth(method = "lm") # reta de regressão linear
-ggsave("reg_exec_2.png", width=6, height=4, units="in")
+exercicio_v2 <- na.omit(exercicio)
 
+ggplot(data = exercicio_v2, aes(x = political_corruption, y = environmental_performance_index))+ # dev variáveis e eixos
+  geom_point()+ # gráf tipo ponto
+  geom_smooth(method = "lm") # reta de regressão linear
+ggsave("reg_exec_1_v2.png", width=6, height=4, units="in")
+ggplot(data = exercicio_v2, aes(x = total_export, y = environmental_performance_index))+ # dev variáveis e eixos
+  geom_point()+ # gráf tipo ponto
+  geom_smooth(method = "lm") # reta de regressão linear
+ggsave("reg_exec_2_v2.png", width=6, height=4, units="in")
 
 modelo_reg <- lm(environmental_performance_index ~ # VD
                 total_export +           # VIs
                 political_corruption,
-              data = exercicio)
+              data = exercicio_v2)
 summary(modelo_reg)
 
-exercicio$Predito <- predict(modelo_reg)
-exercicio$Residuo <- exercicio$total_export - exercicio$Predito
-exercicio$Predito <- predict(modelo_reg)
-exercicio$Residuo <- exercicio$political_corruption - exercicio$Predito
+exercicio_v2$Predito <- predict(modelo_reg)
+exercicio_v2$Residuo <- exercicio_v2$total_export - exercicio_v2$Predito
+exercicio_v2$Predito <- predict(modelo_reg)
+exercicio_v2$Residuo <- exercicio_v2$political_corruption - exercicio_v2$Predito
 
-summary(exercicio$Residuo)
+summary(exercicio_v2$Residuo)
 
 # gráfico de dispersao dos valores preditos x residuos
-ggplot(data = exercicio, aes(x = Predito, y = Residuo)) +
+ggplot(data = exercicio_v2, aes(x = Predito, y = Residuo)) +
   geom_point() +
   labs( x ="Valores Preditos (modelo_reg)", y = "Residuos (modelo_reg)") +
   geom_hline(yintercept = 0, linetype = "dashed", color = "blue")
-ggsave("hom_exec.png", width=6, height=4, units="in")
+ggsave("hom_exec_v2.png", width=6, height=4, units="in")
+
+
+#### fazendo o exercicio_v3
+
+library(readr)
+library(stringr)
+library(stargazer)
+library(ggplot2)
+library(dplyr)
+install.packages("tidyr")
+library(tidyr)
+exercicio <- read.csv("QOGdataSave.csv", sep =";", stringsAsFactors=FALSE)
+exercicio_v3 <- drop_na(exercicio, total_export, political_corruption, environmental_performance_index) #apaga os missing cases das variáveis de interesse
+
+ggplot(data = exercicio_v3, aes(x = political_corruption, y = environmental_performance_index))+ # dev variáveis e eixos
+  geom_point()+ # gráf tipo ponto
+  geom_smooth(method = "lm") # reta de regressão linear
+ggsave("reg_exec_1_v3.png", width=6, height=4, units="in")
+ggplot(data = exercicio_v3, aes(x = total_export, y = environmental_performance_index))+ # dev variáveis e eixos
+  geom_point()+ # gráf tipo ponto
+  geom_smooth(method = "lm") # reta de regressão linear
+ggsave("reg_exec_2_v3.png", width=6, height=4, units="in")
+
+modelo_reg <- lm(environmental_performance_index ~ # VD
+                   total_export +           # VIs
+                   political_corruption,
+                 data = exercicio_v3)
+summary(modelo_reg)
+
+exercicio_v3$Predito <- predict(modelo_reg)
+exercicio_v3$Residuo <- exercicio_v3$total_export - exercicio_v3$Predito
+exercicio_v3$Predito <- predict(modelo_reg)
+exercicio_v3$Residuo <- exercicio_v3$political_corruption - exercicio_v3$Predito
+
+summary(exercicio_v3$Residuo)
+
+# gráfico de dispersao dos valores preditos x residuos
+ggplot(data = exercicio_v3, aes(x = Predito, y = Residuo)) +
+  geom_point() +
+  labs( x ="Valores Preditos (modelo_reg)", y = "Residuos (modelo_reg)") +
+  geom_hline(yintercept = 0, linetype = "dashed", color = "blue")
+ggsave("hom_exec_v3.png", width=6, height=4, units="in")
+
+
+exercicio_v4 <- exercicio_v3[,c(4,8,9)]
+
+ggplot(data = exercicio_v4, aes(x = political_corruption, y = environmental_performance_index))+ # dev variáveis e eixos
+  geom_point()+ # gráf tipo ponto
+  geom_smooth(method = "lm") # reta de regressão linear
+
+ggsave("reg_exec_1_v4.png", width=6, height=4, units="in")
+ggplot(data = exercicio_v4, aes(x = total_export, y = environmental_performance_index))+ # dev variáveis e eixos
+  geom_point()+ # gráf tipo ponto
+  geom_smooth(method = "lm") # reta de regressão linear
+ggsave("reg_exec_2_v4.png", width=6, height=4, units="in")
+
+modelo_reg <- lm(environmental_performance_index ~ # VD
+                   total_export +           # VIs
+                   political_corruption,
+                 data = exercicio_v4)
+summary(modelo_reg)
+
+exercicio_v4$Predito <- predict(modelo_reg)
+exercicio_v4$Residuo <- exercicio_v4$total_export - exercicio_v4$Predito
+exercicio_v4$Predito <- predict(modelo_reg)
+exercicio_v4$Residuo <- exercicio_v4$political_corruption - exercicio_v4$Predito
+
+summary(exercicio_v4$Residuo)
+
+# gráfico de dispersao dos valores preditos x residuos
+ggplot(data = exercicio_v4, aes(x = Predito, y = Residuo)) +
+  geom_point() +
+  labs( x ="Valores Preditos (modelo_reg)", y = "Residuos (modelo_reg)") +
+  geom_hline(yintercept = 0, linetype = "dashed", color = "blue")
+ggsave("hom_exec_v4.png", width=6, height=4, units="in")
